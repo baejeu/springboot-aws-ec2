68a356f44016b12ee24dc1bba8054c858d5da158
baejeu12 <baejeu@naver.com>
2023-05-16 21:18:47 +0900

    "복습 시작"

---
+++ src/test/java/com/example/springbootawsec2/SpringbootAwsEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootawsec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsEc2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
